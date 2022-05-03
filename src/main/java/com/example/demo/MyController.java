8b9f6334df4f5e9c07f9fc2590495eec40b00365
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-05-03 12:42:04 +0200

    add

---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * *
+ * <p>Created by irina on 5/3/2022.</p>
+ * <p>Project: demo</p>
+ * *
+ */
+@Controller
+@RequestMapping("/home")
+public class MyController {
+
+    @GetMapping("/")
+    public String home(ModelMap model) {
+        model.addAttribute("home_attr1", "home_attr1");
+        return "home";
+    }
+
+    @RequestMapping("/test")
+    public String test(ModelMap model) {
+        model.addAttribute("test_attr1", "test_attr1");
+        return "test";
+    }
+}
